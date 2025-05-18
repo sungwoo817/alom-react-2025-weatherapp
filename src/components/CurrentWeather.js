3eddea9c1d8f4e4ceefa9aa665d64af073001a77
rms0817 <rms0817@naver.com>
2025-05-18 16:57:20 +0900

    Update CurrentWeather.js

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -8,10 +8,17 @@ import { getWeatherDescription } from "../utils/weather";
 
 const CurrentWeather = ({ weatherData, isLoading }) => {
   if (isLoading) {
-    return <div>채워주세요</div>;
+    return <div>Loading...</div>;
   }
 
-  return <div>채워주세요</div>;
+  const currentWeather = weatherData.hourly;
+
+  return 
+  <div>
+    <h2>현재 날씨</h2>
+    <p>기온: {currentWeather.temperature_2m[0]}°C</p>
+    <p>날씨: {currentWeather.weather_code ? currentWeather.weather_code[0] : "알 수 없음"}</p>
+  </div>;
 };
 
 export default CurrentWeather;
